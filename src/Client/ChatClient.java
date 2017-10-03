fd9e940e8e4b19d55491d2d27aac7b753f864696
Solnik <ryan.solnik@gentex.com>
2017-10-02 22:05:39 -0400

    Work on 10/2 w/ the danimal

--- src/Client/ChatClient.java
+++ src/Client/ChatClient.java
@@ -1,11 +1,95 @@
 package Client;
 
+import Presence.PresenceService;
+
+import java.io.*;
+import java.math.BigDecimal;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ChatClient {
 
-    boolean busy = false;
+    public static void main(String args[]) {
 
-    public static void main(String args[]){
+        boolean status = true;
+        boolean running = true;
+        String option;
+        String username = args[0];
+        String host = "localhost";
+        Integer port = 1099;
 
+        if (args.length == 2) {
+            String[] hostAndPort = args[1].split(":");
+
+            if (hostAndPort.length == 2) {
+                host = hostAndPort[0];
+                port = Integer.parseInt(hostAndPort[1]);
+            }
+            else if (hostAndPort.length == 1) {
+                host = hostAndPort[0];
+            }
+
+
+        }
+        System.out.println(host +" "+ port);
+
+//            if (System.getSecurityManager() == null) {
+//                System.setSecurityManager(new SecurityManager());
+//            }
+//
+//            BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+//
+//            try {
+//                String name = "PresenceServer";
+//                Registry registry = LocateRegistry.getRegistry(args[1]);
+//                PresenceService server = (PresenceService) registry.lookup(name);
+//
+//                //creates new reginfo object for this client
+//                RegistrationInfo user = new RegistrationInfo(username, host, port, status);
+//                //registers with RMIregistry
+//                server.register(user);
+//
+//
+//                //Menu Structure
+//                while (running) {
+//
+//                    printGreeting();
+//
+//                    option = is.readLine();
+//                    String[] tokens = option.split(" ");
+//
+//                    switch (tokens[0]) {
+//                        case "friends":
+//
+//
+//                            break;
+//                        case "talk":
+//
+//                            break;
+//
+//                        case "broadcast":
+//                            break;
+//
+//                        case "busy":
+//
+//                            break;
+//                        case "available":
+//
+//                            break;
+//                        case "exit":
+//
+//                            break;
+//
+//                        default:
+//                            System.out.println("Invalid command.");
+//                            break;
+//                    }
+//                }
+//            } catch (Exception e) {
+//                System.err.println("Client exception:");
+//                e.printStackTrace();
         /*
         Register name with Arg[1]
         display user interface
@@ -32,4 +116,20 @@ public class ChatClient {
         6.)Exit
          */
     }
+
 }
+//    }
+//
+//    private static void printGreeting() {
+//
+//        System.out.println("Please make a selection from the following items\n" +
+//                "friends - get list of friends available\n" +
+//                "talk.) Compute Prime Numbers\n" +
+//                "broadcast\n" +
+//                "busy\n" +
+//                "available\n" +
+//                "exit\n");
+//    }
+//
+//}
+
--- src/PresenceService/PresenceServerImpl.java
+++ src/PresenceService/PresenceServerImpl.java
@@ -10,56 +10,63 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.Hashtable;
 import java.util.Vector;
 
-public class Server implements PresenceService {
+public class PresenceServerImpl implements PresenceService {
 
-    Hashtable registeredUsers = new Hashtable();
+    Hashtable<String, RegistrationInfo> registeredUsers = new Hashtable();
 
     public static void main(String[] args) {
         if (System.getSecurityManager() == null) {
             System.setSecurityManager(new SecurityManager());
         }
         try {
-            String name = "PresenceService";
-            PresenceService service = new Server();
+            String name = "PresenceServer";
+            PresenceService service = new PresenceServerImpl();
             PresenceService stub = (PresenceService) UnicastRemoteObject.exportObject(service, 0);
             Registry registry = LocateRegistry.getRegistry();
             registry.rebind(name, stub);
-            System.out.println("PresenceService bound");
+            System.out.println("PresenceServer bound");
         } catch (Exception e) {
-            System.err.println("PresenceService exception:");
+            System.err.println("PresenceServer exception:");
             e.printStackTrace();
         }
     }
 
     public boolean register(RegistrationInfo reg) throws RemoteException {
-        //check if user exists in hashtable
 
-        //if not register
-        if (!registeredUsers.containsValue(reg.getUserName())) {
-            registeredUsers.put(reg.getUserName(),reg);
+        if (!registeredUsers.containsKey(reg.getUserName())) {
+            registeredUsers.put(reg.getUserName(), reg);
+            System.out.println("User " + reg.getUserName() + "registered!!!");
             return true;
         }
+        System.out.println("Duplicate Entry!");
         return false;
     }
 
     public boolean updateRegistrationInfo(RegistrationInfo reg) throws RemoteException {
 
 
-        //get user from hash table
-        registeredUsers.get(reg.getUserName());
-        //change value of
+        if (registeredUsers.containsKey(reg.getUserName())) {
+            registeredUsers.put(reg.getUserName(), reg);
+            System.out.println("User " + reg.getUserName() + "Updated!!!");
+            return true;
+        }
+        System.out.println("User " + reg.getUserName() + "does not exist!!!");
         return false;
     }
 
     public void unregister(String userName) throws RemoteException {
-        registeredUsers.get(userName);
+        registeredUsers.remove(userName);
+        System.out.println("Removed " + userName + " from registry!");
     }
 
     public RegistrationInfo lookup(String name) throws RemoteException {
-        return null;
+        return registeredUsers.get(name);
+
     }
 
     public Vector<RegistrationInfo> listRegisteredUsers() throws RemoteException {
-        return null;
+       Vector<RegistrationInfo> userList = new Vector<>();
+       userList.addAll(registeredUsers.values());
+        return userList;
     }
 }
